ac4891ce42724eefcc6cb469e9fedc49b8451081
GeovanedosSantos <geovanemartins45776@gmail.com>
2026-09-20 19:15:33 -0300

    Primeiro Commit criando as pastas do projeto

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+
+function App()  {
+}
+
+export default App
